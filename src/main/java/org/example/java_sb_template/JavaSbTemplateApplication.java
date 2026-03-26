f6dbedc21f5f8b7147b7d9bbe26986bba96bd89a
kangbj1026 <kangbj1026@entwiz.com>
2026-03-26 19:35:41 +0900

    First commit

---
+++ src/main/java/org/example/java_sb_template/JavaSbTemplateApplication.java
@@ -0,0 +1,13 @@
+package org.example.java_sb_template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSbTemplateApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSbTemplateApplication.class, args);
+	}
+	
+}
---
+++ src/test/java/org/example/java_sb_template/JavaSbTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.java_sb_template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSbTemplateApplicationTests {
+	
+	@Test
+	void contextLoads() {
+	}
+	
+}
